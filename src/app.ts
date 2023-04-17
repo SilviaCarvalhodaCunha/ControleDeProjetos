dbb09c0a6af1a3041482e3b61a25a8c2c4e9e62a
silviacarvalhodacunha <silviacarvalhodacunha@gmail.com>
2023-04-17 03:07:41 -0300

    feat: .sql and routes app

--- src/app.ts
+++ src/app.ts
@@ -2,5 +2,19 @@ import express, { Application } from "express";
 import "dotenv/config";
 
 const app: Application = express();
+app.use(express.json())
+
+app.post('/developers', )
+app.get('/developers/:id', )
+app.patch('/developers/:id', )
+app.delete('/developers/:id', )
+app.post('/developers/:id/infos', )
+
+app.post('/projects', )
+app.get('/projects/:id', )
+app.patch('/projects/:id', )
+app.delete('/projects/:id', )
+app.post('/projects/:id/technologies', )
+app.delete('/projects/:id/technologies/:name', )
 
 export default app;
